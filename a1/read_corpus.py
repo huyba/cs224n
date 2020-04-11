73bb428205c07fb25099d0fd5389aa292db2d1a4
Huy Bui <anhhuybk@gmail.com>
2020-04-11 10:56:08 -0700

    use std imports

--- a1/read_corpus.py
+++ a1/read_corpus.py
@@ -1,32 +1,4 @@
-# All Import Statements Defined Here
-# Note: Do not add to this list.
-# ----------------
-
-import sys
-assert sys.version_info[0]==3
-assert sys.version_info[1] >= 5
-
-from gensim.models import KeyedVectors
-from gensim.test.utils import datapath
-import pprint
-import matplotlib.pyplot as plt
-plt.rcParams['figure.figsize'] = [10, 5]
-import nltk
-nltk.download('reuters')
-from nltk.corpus import reuters
-import numpy as np
-import random
-import scipy as sp
-from sklearn.decomposition import TruncatedSVD
-from sklearn.decomposition import PCA
-
-START_TOKEN = '<START>'
-END_TOKEN = '<END>'
-
-np.random.seed(0)
-random.seed(0)
-# ----------------
-
+from std_imports import *
 
 def read_corpus(category="crude"):
     """ Read files from the specified Reuter's category.
